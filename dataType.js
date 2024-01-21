3bcaaec49f61b2a664174a48f6536951bc19ac5d
kanonkartik <kart00ik@gmail.com>
2024-01-21 14:20:35 +0530

    02

---
+++ dataType.js
@@ -0,0 +1,13 @@
+//number=> 2 to power 53
+// bigint
+//string=>""
+//boolean=>true/false
+//null=>stand alone value 
+/*It's important to note that in JavaScript, the typeof null is "object," which is a historical quirk. It doesn't mean that null is an object; it's a primitive value. The typeof operator behavior with null is considered a mistake in the language, but it has been preserved for compatibility reasons.*/
+// undefiend
+// symbol =>unique
+
+//object
+console.log(typeof undefined)//undefine
+
+console.log(typeof null)// object
