c809fea5d28d736ae5714b7bb960a5df952b8f84
kanonkartik <kart00ik@gmail.com>
2024-01-22 09:10:46 +0530

    005

---
+++ string.js
@@ -0,0 +1,268 @@
+let string = new String("yoyo.honey.shingh")
+console.log(string)
+let string4=string.indexOf("shingh")
+console.log(string4)
+let string5=string.split('.',2)
+console.log(string5)
+ let string6=string.join('/',2)
+ console.log(string6)
+ /* 
+ let string = new String("yoyo.honey.shingh")
+console.log(string)
+VM461:2 
+String {'yoyo.honey.shingh'}
+0
+: 
+"y"
+1
+: 
+"o"
+2
+: 
+"y"
+3
+: 
+"o"
+4
+: 
+"."
+5
+: 
+"h"
+6
+: 
+"o"
+7
+: 
+"n"
+8
+: 
+"e"
+9
+: 
+"y"
+10
+: 
+"."
+11
+: 
+"s"
+12
+: 
+"h"
+13
+: 
+"i"
+14
+: 
+"n"
+15
+: 
+"g"
+16
+: 
+"h"
+length
+: 
+17
+[[Prototype]]
+: 
+String
+anchor
+: 
+ƒ anchor()
+at
+: 
+ƒ at()
+big
+: 
+ƒ big()
+blink
+: 
+ƒ blink()
+bold
+: 
+ƒ bold()
+charAt
+: 
+ƒ charAt()
+charCodeAt
+: 
+ƒ charCodeAt()
+codePointAt
+: 
+ƒ codePointAt()
+concat
+: 
+ƒ concat()
+constructor
+: 
+ƒ String()
+endsWith
+: 
+ƒ endsWith()
+fixed
+: 
+ƒ fixed()
+fontcolor
+: 
+ƒ fontcolor()
+fontsize
+: 
+ƒ fontsize()
+includes
+: 
+ƒ includes()
+indexOf
+: 
+ƒ indexOf()
+isWellFormed
+: 
+ƒ isWellFormed()
+italics
+: 
+ƒ italics()
+lastIndexOf
+: 
+ƒ lastIndexOf()
+length
+: 
+0
+link
+: 
+ƒ link()
+localeCompare
+: 
+ƒ localeCompare()
+match
+: 
+ƒ match()
+matchAll
+: 
+ƒ matchAll()
+normalize
+: 
+ƒ normalize()
+padEnd
+: 
+ƒ padEnd()
+padStart
+: 
+ƒ padStart()
+repeat
+: 
+ƒ repeat()
+replace
+: 
+ƒ replace()
+replaceAll
+: 
+ƒ replaceAll()
+search
+: 
+ƒ search()
+slice
+: 
+ƒ slice()
+small
+: 
+ƒ small()
+split
+: 
+ƒ split()
+startsWith
+: 
+ƒ startsWith()
+strike
+: 
+ƒ strike()
+sub
+: 
+ƒ sub()
+substr
+: 
+ƒ substr()
+substring
+: 
+ƒ substring()
+sup
+: 
+ƒ sup()
+toLocaleLowerCase
+: 
+ƒ toLocaleLowerCase()
+toLocaleUpperCase
+: 
+ƒ toLocaleUpperCase()
+toLowerCase
+: 
+ƒ toLowerCase()
+toString
+: 
+ƒ toString()
+toUpperCase
+: 
+ƒ toUpperCase()
+toWellFormed
+: 
+ƒ toWellFormed()
+trim
+: 
+ƒ trim()
+trimEnd
+: 
+ƒ trimEnd()
+trimLeft
+: 
+ƒ trimStart()
+trimRight
+: 
+ƒ trimEnd()
+trimStart
+: 
+ƒ trimStart()
+valueOf
+: 
+ƒ valueOf()
+Symbol(Symbol.iterator)
+: 
+ƒ [Symbol.iterator]()
+[[Prototype]]
+: 
+Object
+[[PrimitiveValue]]
+: 
+""
+[[PrimitiveValue]]
+: 
+"yoyo.honey.shingh"
+undefined
+let string2=string.slice(0,5);
+undefined
+console.log(string2)
+VM655:1 yoyo.
+undefined
+let string3=string.bold()
+undefined
+console.log(string3)
+VM831:1 <b>yoyo.honey.shingh</b>
+undefined
+let string4=string.indexOf()
+undefined
+console.log(string4)
+
+undefined
+
+let string4=string.indexOf(yoyo.honey.shingh,7)
+
+
+
+let string5=string.split(10,15)
+undefined
+console.log(string5)
+
+﻿
+
+
+ */
+
